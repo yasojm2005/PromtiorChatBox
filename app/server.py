6de4d65593da13fd952adbacc3f3b3fdc4c56ce8
Yasiel Jorge Martínez <yasojm2005@gmail.com>
2026-02-13 10:27:37 -0300

    Add dynamic port

--- app/server.py
+++ app/server.py
@@ -20,5 +20,8 @@ app = FastAPI(title="Promtior RAG Chatbot")
 add_routes(app, rag_chain, path="/rag", input_type=RagInput)
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
